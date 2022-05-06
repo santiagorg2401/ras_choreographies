8784a2b40b35a98faf13dd042e5d99206ad197c5
Santiago Restrepo <sanrega24@gmail.com>
2022-05-06 09:17:24 -0500

    Auto formation script added and collission avoidance call included.

--- scripts/auto_form.py
+++ scripts/auto_form.py
@@ -2,6 +2,7 @@
 import numpy as np
 import sys
 import os
+import rospy
 
 sys.path.append('/home/' + os.getlogin() +
                 '/crazyflie/crazyswarm/ros_ws/src/crazyswarm/scripts')
@@ -10,40 +11,16 @@ os.chdir("/home/" + os.getlogin() +
 
 from pycrazyswarm import *
 
-# Crazyflies list, this is optional, however, it is safer to declare it on code.
-crazyflies_yaml = """
-crazyflies:
-  - id: 1
-    channel: 80
-    initialPosition: [0.5, 0.5, 0.0]
-    type: default
-  - id: 2
-    channel: 80
-    initialPosition: [1.5, 0.5, 0.0]
-    type: default
-  - id: 3
-    channel: 80
-    initialPosition: [1.0, 1.0, 0.0]
-    type: default
-  - id: 4
-    channel: 80
-    initialPosition: [0.5, 1.5, 0.0]
-    type: default
-  - id: 5
-    channel: 80
-    initialPosition: [1.5, 1.5, 0.0]
-    type: default
-"""
 #Collision avoidance (between Crazyflies) parameters.
-xy_radius = 0.25
+xy_radius = 0.30
 radii = xy_radius * np.array([1.0, 1.0, 3.0])
 
 class sender():
-    def __init__(self, speed):
+    def __init__(self, path, speed):
         # Constructor of the class sender.
         # Create the needed objects and initialize a ROS Node.
         # I nstance from the class Crazyswarm in pycrazyswarm.crazyswarm
-        self.swarm = Crazyswarm(crazyflies_yaml=crazyflies_yaml)
+        self.swarm = Crazyswarm()
         # Instance from the class CrazyflieServer in pycrazyswarm.crazyflie
         self.allcfs = self.swarm.allcfs
         # List of objects  from the class Crazyflie in pycrazyswarm.crazyflie, it has as many objects as declared in crazyflies_yaml, in the same order starting from 0.
@@ -51,52 +28,43 @@ class sender():
         # Instance from the class timeHelper in pycrazyswarm.crazyflie
         self.timeHelper = self.swarm.timeHelper
 
+        self.path = path
         self.speed = speed
 
         #Enable collision avoidance between all Crazyflies.
         for i, cf in enumerate(self.cfs):
           others = self.cfs[:i] + self.cfs[(i+1):]
           cf.enableCollisionAvoidance(others, radii)
-        
-        self.cfs[i].setLEDColor(0/255.0, 0/255.0, 0/255.0)
+          
         # Set solid color effect on all  LED-Decks (see https://www.bitcraze.io/documentation/repository/crazyflie-firmware/master/ for more parameters).
-        for cf in self.allcfs.crazyflies:
-            cf.setParam("ring/effect", 7)
-            cf.setParam("ring/headlightEnable", 0)
-            cf.setLEDColor(0/255.0, 0/255.0, 0/255.0)
+        self.led(0)
 
     def publisher(self):
-        # Set up mask.
-        self.cfs[0].setGroupMask(0)
-        self.cfs[1].setGroupMask(0)
-        self.cfs[3].setGroupMask(0)
-        self.cfs[4].setGroupMask(0)
-        
+        # Set up parameters.
+        data = np.genfromtxt(self.path, delimiter=',')
+
         # Confirmation.
         print("Press any button to start with the sequence.")
         self.swarm.input.waitUntilButtonPressed()
+        self.led(1)
 
-        # Take off.
-        self.cfs[2].takeoff(targetHeight=1.5, duration=1.5/self.speed)
-        self.timeHelper.sleep(1.5/self.speed + 1.0)
+        for i in range(0, len(data)):
+            takeOffHeight = data[i][2]
+            initialPos = data[:, 0:3][i] - np.array([0, 0, takeOffHeight])
 
-        self.allcfs.takeoff(targetHeight=1.0, duration=1.0 /
-                            self.speed, groupMask=0)
-                            
-        self.timeHelper.sleep(1.0/self.speed*1.5)
-
-        # Change colour.
-        for cf in self.allcfs.crazyflies:
-            cf.setLEDColor(255/255.0, 0/255.0, 0/255.0)
-            cf.setParam("ring/headlightEnable", 1)
-
+            # Take off.
+            self.cfs[i].takeoff(targetHeight=takeOffHeight,
+                                duration=takeOffHeight/self.speed)
+            self.timeHelper.sleep(takeOffHeight/self.speed*1.2)
+        
         # Confirmation.
         print("Press any button to finish the sequence.")
         self.swarm.input.waitUntilButtonPressed()
 
         # Land.
-        self.allcfs.land(targetHeight=0.02, duration=1.5/self.speed)
-        self.timeHelper.sleep(1.5/self.speed*1.5)
+        self.allcfs.land(targetHeight=0.02, duration=(
+            takeOffHeight)/speed)
+        self.timeHelper.sleep((takeOffHeight)/speed*1.2)
 
     def calculateTime(self, speed, initialPos, finalPos):
         distance = abs(np.linalg.norm(finalPos - initialPos))
@@ -104,9 +72,17 @@ class sender():
 
         return Time
 
+    def led(self, enable):
+        # Change colour.
+        for cf in self.allcfs.crazyflies:
+            cf.setLEDColor(255*enable/255.0, 0/255.0, 0/255.0)
+            cf.setParam("ring/headlightEnable", enable)
+
 
 if __name__ == "__main__":
-    speed = 1.0
+    args = rospy.myargv(argv=sys.argv)
+    file_path = args[1]
+    speed = 0.6
 
-    sen = sender(speed=speed)
+    sen = sender(path=file_path, speed=speed)
     sen.publisher()
