033f8ec8c31efd7032a46e29aa8dc80e4ed607f0
Santiago Restrepo <sanrega24@gmail.com>
2022-05-31 20:29:27 -0500

    Emergency stop added for more security.

--- scripts/auto_form.py
+++ scripts/auto_form.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import sys
 import os
@@ -11,9 +12,10 @@ os.chdir("/home/" + os.getlogin() +
 
 from pycrazyswarm import *
 
-#Collision avoidance (between Crazyflies) parameters.
+# Collision avoidance (between Crazyflies) parameters.
 xy_radius = 0.30
 radii = xy_radius * np.array([1.0, 1.0, 3.0])
+
 
 class sender():
     def __init__(self, path, speed):
@@ -31,11 +33,11 @@ class sender():
         self.path = path
         self.speed = speed
 
-        #Enable collision avoidance between all Crazyflies.
+        # Enable collision avoidance between all Crazyflies.
         for i, cf in enumerate(self.cfs):
-          others = self.cfs[:i] + self.cfs[(i+1):]
-          cf.enableCollisionAvoidance(others, radii)
-          
+            others = self.cfs[:i] + self.cfs[(i+1):]
+            cf.enableCollisionAvoidance(others, radii)
+
         # Set solid color effect on all  LED-Decks (see https://www.bitcraze.io/documentation/repository/crazyflie-firmware/master/ for more parameters).
         self.led(0)
 
@@ -57,9 +59,9 @@ class sender():
                                 duration=takeOffHeight/self.speed)
             self.timeHelper.sleep(takeOffHeight/self.speed*1.2)
 
-            self.cfs[i].goTo(data[:,0:3][i], 0, 1)
+            self.cfs[i].goTo(data[:, 0:3][i], 0, 1)
             self.timeHelper.sleep(1.2)
-        
+
         # Confirmation.
         print("Press any button to finish the sequence.")
         self.swarm.input.waitUntilButtonPressed()
@@ -83,9 +85,16 @@ class sender():
 
 
 if __name__ == "__main__":
-    args = rospy.myargv(argv=sys.argv)
-    file_path = args[1]
-    speed = 0.6
+    try:
+        args = rospy.myargv(argv=sys.argv)
+        file_path = args[1]
+        speed = 0.6
 
-    sen = sender(path=file_path, speed=speed)
-    sen.publisher()
+        sen = sender(path=file_path, speed=speed)
+        sen.publisher()
+
+    except KeyboardInterrupt:
+        print("Emergency cancel registred, landing UAVs.")
+        sen.allcfs.land(targetHeight = 0.02, duration = 5)
+        sen.timeHelper.sleep(5*1.2)
+        sen.allcfs.emergency()
--- scripts/auto_lp.py
+++ scripts/auto_lp.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import sys
 import os
 import rospy
 
-sys.path.append('/home/'+ os.getlogin() + '/crazyflie/crazyswarm/ros_ws/src/crazyswarm/scripts')
-os.chdir("/home/" + os.getlogin() + "/crazyflie/crazyswarm/ros_ws/src/crazyswarm/scripts/")
+sys.path.append('/home/' + os.getlogin() +
+                '/crazyflie/crazyswarm/ros_ws/src/crazyswarm/scripts')
+os.chdir("/home/" + os.getlogin() +
+         "/crazyflie/crazyswarm/ros_ws/src/crazyswarm/scripts/")
 
 from pycrazyswarm import *
 
@@ -13,10 +16,14 @@ class sender():
     def __init__(self, path, speed):
       # Constructor of the class sender.
       # Create the needed objects and initialize a ROS Node.
-      self.swarm = Crazyswarm()  #I nstance from the class Crazyswarm in pycrazyswarm.crazyswarm
-      self.allcfs = self.swarm.allcfs                           # Instance from the class CrazyflieServer in pycrazyswarm.crazyflie
-      self.cfs = self.allcfs.crazyflies                         # List of objects  from the class Crazyflie in pycrazyswarm.crazyflie, it has as many objects as declared in crazyflies_yaml, in the same order starting from 0.
-      self.timeHelper = self.swarm.timeHelper                   # Instance from the class timeHelper in pycrazyswarm.crazyflie
+      # I nstance from the class Crazyswarm in pycrazyswarm.crazyswarm
+      self.swarm = Crazyswarm()
+      # Instance from the class CrazyflieServer in pycrazyswarm.crazyflie
+      self.allcfs = self.swarm.allcfs
+      # List of objects  from the class Crazyflie in pycrazyswarm.crazyflie, it has as many objects as declared in crazyflies_yaml, in the same order starting from 0.
+      self.cfs = self.allcfs.crazyflies
+      # Instance from the class timeHelper in pycrazyswarm.crazyflie
+      self.timeHelper = self.swarm.timeHelper
 
       self.path = path
       self.speed = speed
@@ -27,7 +34,7 @@ class sender():
     def publisher(self):
       # Set up parameters.
       data = np.genfromtxt(self.path, delimiter=',')
-      
+
       takeOffHeight = data[0][2]
       initialPos = data[:, 0:3][0] - np.array([0, 0, takeOffHeight])
 
@@ -36,7 +43,8 @@ class sender():
       self.swarm.input.waitUntilButtonPressed()
 
       # Take off.
-      self.allcfs.takeoff(targetHeight=takeOffHeight, duration=takeOffHeight/self.speed)
+      self.allcfs.takeoff(targetHeight=takeOffHeight,
+                          duration=takeOffHeight/self.speed)
       self.timeHelper.sleep(takeOffHeight/self.speed*1.2)
 
       self.led(1)
@@ -55,15 +63,17 @@ class sender():
 
           for cf in self.allcfs.crazyflies:
             if i == 0:
-              Time = self.calculateTime(self.speed, initialPos, data[:, 0:3][i])
+              Time = self.calculateTime(
+                  self.speed, initialPos, data[:, 0:3][i])
             else:
-              Time = self.calculateTime(self.speed, data[:, 0:3][i - 1], data[:, 0:3][i])
+              Time = self.calculateTime(
+                  self.speed, data[:, 0:3][i - 1], data[:, 0:3][i])
 
             cf.goTo(data[:, 0:3][i], 0, Time)
             self.timeHelper.sleep(Time*1.5)
 
-      #Land.
-      self.allcfs.land(targetHeight=0.02, duration = (data[-1][2] - 0.02)/speed)
+      # Land.
+      self.allcfs.land(targetHeight=0.02, duration=(data[-1][2] - 0.02)/speed)
       self.timeHelper.sleep((data[-1][2] - 0.02)/speed*1.2)
 
     def calculateTime(self, speed, initialPos, finalPos):
@@ -78,10 +88,17 @@ class sender():
         cf.setLEDColor(255*enable/255.0, 0/255.0, 0/255.0)
         cf.setParam("ring/headlightEnable", enable)
 
-if __name__ == "__main__":
-  args = rospy.myargv(argv=sys.argv)
-  file_path = args[1]
-  speed = 0.6
 
-  sen = sender(path=file_path, speed=speed)
-  sen.publisher()
+if __name__ == "__main__":
+  try:
+    args = rospy.myargv(argv=sys.argv)
+    file_path = args[1]
+    speed = 0.6
+
+    sen = sender(path=file_path, speed=speed)
+    sen.publisher()
+  except KeyboardInterrupt:
+    print("Emergency cancel registred, landing UAVs.")
+    sen.allcfs.land(targetHeight = 0.02, duration = 5)
+    sen.timeHelper.sleep(5*1.2)
+    sen.allcfs.emergency()
